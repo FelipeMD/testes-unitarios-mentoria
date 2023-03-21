28e0d53d41082d8eab57d78c15aacad44775d7a9
fesomaz <felipe.macieldsouza@gmail.com>
2023-03-21 16:27:20 -0300

    Initial commit

---
+++ TestProject1/WeatherForecastControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WebApplication1;
+using WebApplication1.Controllers;
+
+namespace TestProject1;
+
+public class WeatherForecastControllerTest
+{
+    private readonly WeatherForecastController _controller;
+
+    public WeatherForecastControllerTest()
+    {
+        var logger = Mock.Of<ILogger<WeatherForecastController>>();
+        _controller = new WeatherForecastController(logger);
+    }
+
+    [Fact]
+    public void get_previsao_tempo_com_sucesso()
+    {
+        //Arrange(preparar)
+        
+        //Act(agir)
+        var result = _controller.Get();
+        
+        //Assert(verificar)
+        Assert.IsAssignableFrom<IEnumerable<WeatherForecast>>(result);
+        var result2 = Assert.IsType<List<WeatherForecast>>(result);
+        Assert.Equal(5, result2.Count);
+    }
+
+    [Fact]
+    public void get_previsao_tempo_com_falha()
+    {
+        //Arrange(preparar)
+        var mockTempo = HttpStatusCode.BadRequest;
+
+        //Act(agir)
+        var result = _controller.Get();
+
+        //Assert(verificar)
+        Assert.IsType<List<WeatherForecast>>(result);
+        var result2 = Assert.IsType<List<WeatherForecast>>(result);
+        Assert.False(result2.All(x => x.TemperatureC < 0));
+    }
+}
